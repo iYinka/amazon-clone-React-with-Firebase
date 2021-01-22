bb93dccdbb8f392f938785319369e605f4664d15
Olayinka Victor Owolabi <olayinkav28@gmail.com>
2021-01-22 10:33:18 +0100

    Clone Amazon with few functionalities

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,40 @@
 const functions = require('firebase-functions');
+const express = require("express");
+const cors = require('cors')
+const stripe = require('stripe')('sk_test_51I8A2cLW6jardt62gcSLEdtAXQrnXjrW5BedtIQwfSBL9uz6aZ1tRRc9opp5ZEJJjD49PVt6GlXCzkZQds4GjSwh00uQpnhThU')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//API
+
+//App config
+const app = express();
+
+
+//Middleware
+app.use(cors({
+  origin: true
+}));
+app.use(express.json());
+
+//Api routes
+app.get('/', (request, response) => response.status(200).send('Hello World!'))
+
+app.post('/payments/create', async (request, response) => {
+  const total = request.query.total
+
+  console.log('PAYMENT REQUEST RECIEVED FOR THIS AMOUNT>>>>>', total)
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, //subunits of currency e.g cents for usd
+    currency: "usd"
+  });
+
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  })
+})
+
+//Listen command
+exports.api = functions.https.onRequest(app) //To run live
+
+//To run the backend locally bdefore deploying run 'firebase emulators:start' and DO NOT CLOSE!!!
+//EXAMPLE ENDPOINT = http://localhost:5001/clone-bbef4/us-central1/api gotten from running firebase emulators:start
--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/clone-bbef4/us-central1/api'  //API URL(cloud function) URL
+  baseURL: 'https://us-central1-clone-bbef4.cloudfunctions.net/api'  //API URL(cloud function) URL
 });
+//To test locally running firebase emulator:start using the example baseURL below 
+//baseURL: 'http://localhost:5001/clone-bbef4/us-central1/api'  //API URL(cloud function) URL
 
 
 export default instance;
